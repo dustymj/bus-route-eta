afdf9ecec157f00348185c9992637c4325f308d9
Dustin <dustymj@msn.com>
2020-03-26 23:19:11 -0500

    03/26/2020: Dustin Johnson - Initial commit, create the bus route eta project.

---
+++ src/main/java/com/dustin/busrouteeta/BusRouteEtaApplication.java
@@ -0,0 +1,13 @@
+package com.dustin.busrouteeta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusRouteEtaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusRouteEtaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dustin/busrouteeta/BusRouteEtaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dustin.busrouteeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusRouteEtaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
