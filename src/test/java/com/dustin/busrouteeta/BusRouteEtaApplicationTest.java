5cb0b741c8e8aea95740a04af8f1eeb210b2d279
Dustin <dustymj@msn.com>
2020-03-27 00:12:02 -0500

    03/26/2020: Dustin Johnson - Remove spring annotations and spring app startup. Do I even need spring for this?

--- src/test/java/com/dustin/busrouteeta/BusRouteEtaApplicationTest.java
+++ src/test/java/com/dustin/busrouteeta/BusRouteEtaApplicationTest.java
@@ -1,13 +1,5 @@
 package com.dustin.busrouteeta;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
 class BusRouteEtaApplicationTest {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
