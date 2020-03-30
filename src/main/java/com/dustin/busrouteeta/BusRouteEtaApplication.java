136dfd393dedac79f15dad1bac9ed08e1098095d
Dustin <dustymj@msn.com>
2020-03-30 02:19:27 -0500

    03/30/2020: Dustin Johnson - Updated main method to call into validation as well as create the timepoint departure object that will be used to hold all of the final, validated data for the final call to metro transit's API.

--- src/main/java/com/dustin/busrouteeta/BusRouteEtaApplication.java
+++ src/main/java/com/dustin/busrouteeta/BusRouteEtaApplication.java
@@ -1,8 +1,37 @@
 package com.dustin.busrouteeta;
 
+import com.dustin.busrouteeta.data.TimepointDepartureVO;
+import com.dustin.busrouteeta.services.InputHandler;
+import com.dustin.busrouteeta.services.InputValidation;
+
+import java.util.HashMap;
+
 public class BusRouteEtaApplication {
 
 	public static void main(String[] args) {
-	}
+        HashMap<String, String> responseMap;
 
+        responseMap = InputHandler.handleInputs(args);
+
+        if (responseMap.size() == 3) {
+            TimepointDepartureVO timepointDepartureVO = new TimepointDepartureVO();
+            boolean valid = InputValidation.validateInputs(responseMap, timepointDepartureVO);
+
+            if (!valid) {
+                System.out.println("Please check your Route, Direction, and Stop to make sure they are correct.");
+                return;
+            }
+
+            System.out.println("Success.");
+
+            // Send call to API
+
+            // Receive services
+
+            // Handle permutations on response
+            //etc. Statuses other than the 200 variety
+
+            // write response to user.
+        }
+	}
 }
